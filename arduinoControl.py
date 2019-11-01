026715951157febd4055fef8f4a701d50bd52e08
Hyunkyung Rho <hrho@olin.edu>
2019-11-01 12:01:14 -0400

    Can control motor speed with python script

--- arduinoControl.py
+++ arduinoControl.py
@@ -16,11 +16,14 @@ def control_motor():
         var = input()
         if var == "1":
             # arduino.write("1".encode('utf-8'))
-            arduino.write("100".encode('utf-8'))
-        if var == "0":
-            arduino.write("0".encode('utf-8'))
+            arduino.write("1".encode())
+            # arduino.write(b"1")
+        elif var == "0":
+            arduino.write("0".encode())
+            # arduino.write(b"0")
         else:
-            arduino.write("0".encode('utf-8'))
+            arduino.write("0".encode())
+            # arduino.write(b"0")
 
 def check_print():
     arduino = start_serial("COM5", 9600)
--- controlMotor/controlMotor.ino
+++ controlMotor/controlMotor.ino
@@ -1,47 +1,38 @@
-// QUESTIONS:
-// 1. Serial.available is always 0
-// 2. readstring is not properly working
-
-
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *motor = AFMS.getMotor(2);
 
-char motorSpeed;
+String motorSpeed;
 
 void setup() {
   AFMS.begin();
   Serial.begin(9600);
-  Serial.print("Ready");
 }
 
 void loop() {
-  Serial.print(" || Serial Value: ");
-  Serial.print(Serial.available());
-
-  while (Serial.available() >= 0) {
-    motorSpeed = Serial.read();
+//  Serial.print(" || Serial Value: ");
+//  Serial.print(Serial.available());
+  
+  while (Serial.available() > 0) {
+    motorSpeed = Serial.readString();
     Serial.print("| Motorspeed: ");
     Serial.print(motorSpeed);
     
-    if (motorSpeed == 1) {
+    if (motorSpeed == "1") {
       motor->setSpeed(80);
       Serial.print("| motor fast");
-//      motor->run(FORWARD);
+      motor->run(FORWARD);
     }
-    else if (motorSpeed == 0) {
+    else if (motorSpeed == "0") {
       motor->setSpeed(40);
       Serial.print("| motor slow");
-//      motor->run(FORWARD);
+      motor->run(FORWARD);
     }
     else {
-      motor->setSpeed(20);
+      motor->setSpeed(10);
       Serial.print("| nothing?");
-//      motor->run(FORWARD);
+      motor->run(FORWARD);
     }
-    motor->run(FORWARD);
   }
-  delay(2);
-  Serial.println("out of while");
 }

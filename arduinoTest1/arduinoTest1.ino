21f3af0c7a2a960d2e332263c2fdbef22ed8e341
Hyunkyung Rho <hrho@olin.edu>
2019-11-03 14:52:42 -0500

    Senses audio input and turns motor on/off

--- arduinoTest1/arduinoTest1.ino
+++ arduinoTest1/arduinoTest1.ino
@@ -1,7 +1,16 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *motor = AFMS.getMotor(1);
+
+int motorSpeed;
+
 char data;
+int analog_val;
 
 void setup() {
   Serial.begin(9600);
+  AFMS.begin();
   pinMode(LED_BUILTIN, OUTPUT);
             
 }
@@ -11,17 +20,29 @@ void loop() {
     data = Serial.readString().toInt();
     
     if (data == 1) {
-      Serial.print("LED On");
+      motorSpeed = 255;
+      motor->setSpeed(motorSpeed);
+      motor->run(FORWARD);
+//      Serial.print("LED On");
       digitalWrite(LED_BUILTIN, HIGH);
       delay(1000);
     }
     else if (data == 0) {
-      Serial.print("LED Off");
+      motorSpeed = 0;
+      motor->setSpeed(motorSpeed);
+      motor->run(FORWARD);
+//      Serial.print("LED Off");
       digitalWrite(LED_BUILTIN, LOW);
-      delay(1000); 
+      delay(10); 
     }
     else {
+      motor->setSpeed(100);
+      motor->run(FORWARD);
       digitalWrite(LED_BUILTIN, LOW);
     }
   }
+  analog_val = analogRead(A3);
+  Serial.println(analog_val); 
+  
+  delay(100);
 }
--- communicateArduino.py
+++ communicateArduino.py
@@ -7,16 +7,28 @@ def start_serial(com,baud):
     return serialPort
 
 def control_LED():
-    arduino = start_serial("COM5", 9600, timeout=1)
+    arduino = start_serial("COM5", 9600)
 
     while True:
-        data = arduino.readline().decode() 
-        print(data)
-        print("Enter: ")
-        var = input()
-        if var == "1":
-            arduino.write("1".encode())
-        elif var == "0":
-            arduino.write("0".encode())
+        # data = arduino.readline().decode("latin-1") 
+        print("---")
+        data = arduino.readline().decode().strip("\r\n")
+        # print(data)
+        # print("Enter: ")
+        # var = input()
+        # if var == "1":
+        try:
+            if int(data) >= 500:
+                # print("SUCCESS")
+                print(data)
+                arduino.write("1".encode())
+            # elif var == "0":
+            elif int(data) < 500: 
+                # print("SUCCESS")
+                print(data)
+                arduino.write("0".encode())
+        except ValueError:
+            print("FAIL")
+            pass
 
 control_LED()
